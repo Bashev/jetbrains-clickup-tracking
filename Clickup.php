e2d8b32242e3672363dc5dfccf5eba32236aea9d
Kostadin Bashev <bashev@webcode.bg>
2023-02-25 05:13:47 +0200

    Initial Commit

---
+++ Clickup.php
@@ -0,0 +1,222 @@
+<?php
+
+class Clickup
+{
+    const CLICKUP_API_URL = 'https://api.clickup.com/api/v2/';
+
+    private object $user;
+
+    private array $teams = [];
+
+    private array $spaces = [];
+
+    private array $tasks = [];
+
+    private int $spaceId;
+
+    public function __construct()
+    {
+        if (isset($_SERVER['HTTP_PRIVATE_TOKEN']) && $response = $this->apiCall('user')) {
+            if (!empty($response) && isset($response->user->id)) {
+                $this->user = $response->user;
+            }
+        }
+
+        if (empty($this->user)) {
+            header('HTTP/1.0 403 Forbidden');
+            exit();
+        }
+    }
+
+    private function apiCall(string $url, array $params = [])
+    {
+        $curl = curl_init();
+        $options = [
+            CURLOPT_URL            => self::CLICKUP_API_URL . $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING       => '',
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST  => !empty($params) ? 'POST' : 'GET',
+            CURLOPT_HTTPHEADER     => [
+                'Content-Type: application/json',
+                'Authorization: ' . $_SERVER['HTTP_PRIVATE_TOKEN']
+            ],
+        ];
+
+        if (!empty($params)) {
+            $options[CURLOPT_POSTFIELDS] = json_encode($params);
+        }
+
+        curl_setopt_array($curl, $options);
+
+        $response = curl_exec($curl);
+        if (curl_getinfo($curl, CURLINFO_HTTP_CODE) === 200) {
+            curl_close($curl);
+
+            return json_decode($response);
+        }
+
+
+        curl_close($curl);
+
+        return json_decode($response);
+    }
+
+    /**
+     * @return object
+     */
+    public function getUser(): object
+    {
+        return $this->user;
+    }
+
+    /**
+     * Get All Teams (Companies) on which user is added.
+     *
+     * @return array
+     */
+    private function getTeams(): array
+    {
+        if (empty($this->teams)) {
+            $userTeams = [];
+            $teams = $this->apiCall('team');
+            if (isset($teams->teams)) {
+                foreach ($teams->teams as $team) {
+                    if (isset($team->id) && isset($team->members)) {
+                        foreach ($team->members as $member) {
+                            if ($member->user->email == $this->user->email) {
+                                $userTeams[] = $team->id;
+                            }
+                        }
+                    }
+                }
+            }
+
+            $this->teams = $userTeams;
+        }
+
+        return $this->teams;
+    }
+
+    /**
+     * Get All Spaces to logged user.
+     *
+     * @return array
+     */
+    public function getSpaces(): array
+    {
+        if (empty($this->spaces)) {
+            $userSpaces = [];
+            foreach ($this->getTeams() as $userTeam) {
+                $spaces = $this->apiCall(sprintf('team/%s/space', $userTeam));
+                if (isset($spaces->spaces)) {
+                    foreach ($spaces->spaces as $space) {
+                        $space->team_id = $userTeam;
+                        $userSpaces[$space->id] = $space;
+                    }
+                }
+            }
+
+            $this->spaces = $userSpaces;
+        }
+
+        return $this->spaces;
+    }
+
+    public function setSpaceId(int $spaceId): void
+    {
+        $this->spaceId = $spaceId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpaceId(): int
+    {
+        return $this->spaceId;
+    }
+
+    private function getCurrentSpace(): ?object
+    {
+        if (empty($this->spaces)) {
+            $this->getSpaces();
+        }
+
+        return $this->spaces[$this->getSpaceId()] ?? null;
+    }
+
+    /**
+     * Get Lists related to user spaces.
+     *
+     * @return array
+     */
+    private function getLists(): array
+    {
+        $userLists = [];
+        $lists = $this->apiCall(sprintf('space/%s/list', $this->getSpaceId()));
+        if (isset($lists->lists)) {
+            foreach ($lists->lists as $list) {
+                $userLists[] = $list->id;
+            }
+        }
+
+        return $userLists;
+    }
+
+    /**
+     * Get All tasks for user.
+     *
+     * @return array
+     */
+    public function getTasks(): array
+    {
+        if (empty($this->tasks)) {
+            $userTasks = [];
+            $queryParams = http_build_query([
+                'include_closed' => !isset($_GET['state']) || $_GET['state'] !== 'opened',
+                'archived'       => false,
+                'subtasks'       => true,
+                'assignees'      => [$this->user->id]
+            ]);
+
+            foreach ($this->getLists() as $userList) {
+                $tasks = $this->apiCall(sprintf('list/%s/task?%s', $userList, $queryParams));
+                if (isset($tasks->tasks)) {
+                    $userTasks = array_merge($userTasks, $tasks->tasks);
+                }
+            }
+
+            $this->tasks = $userTasks;
+        }
+
+        return $this->tasks;
+    }
+
+    public function addTracking($taskId, $duration)
+    {
+        if ($space = $this->getCurrentSpace()) {
+            $tracking = [
+                'description' => 'Automatically tracked by PHPStorm',
+                'tags'        => [
+                    [
+                        'name'   => 'Development',
+                        'tag_bg' => '#BF55EC',
+                        'tag_fg' => '#BF55EC'
+                    ]
+                ],
+                'start'       => round(microtime(true)*1000) - $duration,
+                'billable'    => true,
+                'duration'    => $duration,
+                'assignee'    => $this->user->id,
+                'tid'         => $taskId
+            ];
+
+            return $this->apiCall(sprintf('team/%s/time_entries', $space->team_id), $tracking);
+        }
+
+        return false;
+    }
+}
---
+++ index.php
@@ -0,0 +1,175 @@
+<?php
+
+ini_set('display_errors', 1);
+
+header('Content-type: application/json');
+
+require_once 'Clickup.php';
+
+const STORAGE_DIR = 'storage';
+
+$clickup = new Clickup();
+
+// All Gitlab Urls.
+if (preg_match('@^/api/v4/(\w+)(/((\w+)%2F)?(\d+)/(issues))?(/(\d+)/(add_spent_time))?@', $_SERVER['REQUEST_URI'], $matches) && isset($matches[1])) {
+    $page = $_GET['page'] ?? 1;
+    $perPage = $_GET['per_page'] ?? 30;
+
+    // Version
+    if ($matches[1] === 'version') {
+        $response = [
+            "version"    => "15.8.1",
+            "enterprise" => false
+        ];
+
+        echo json_encode($response);
+        exit;
+    }
+
+    // Projects
+    if (count($matches) === 2 && $matches[1] === 'projects') {
+        $spaces = paginate($clickup->getSpaces(), $page, $perPage);
+        if (!empty($spaces)) {
+            $response = [];
+            foreach ($spaces as $space) {
+                $response[] = [
+                    'id'      => $space->id,
+                    'name'    => $space->name,
+                    'web_url' => sprintf('%s://%s/api/space/%s',
+                        $_SERVER['REQUEST_SCHEME'],
+                        $_SERVER['SERVER_NAME'],
+                        $space->id)
+                ];
+            }
+
+            echo json_encode($response);
+        }
+        exit;
+    }
+
+    // Issues (Tasks)
+    if (count($matches) === 7 && $matches[6] === 'issues' && empty($matches[3]) && empty($matches[4])) {
+        $clickup->setSpaceId($matches[5]);
+
+        $tasks = [];
+        $clickupTasks = $clickup->getTasks();
+
+        if (!empty($clickupTasks)) {
+            $taskIds = getTaskIds($matches[5]);
+
+            foreach ($clickupTasks as $task) {
+                // looks TaskID in the storage, if not found, add it.
+                if (!in_array($task->id, $taskIds)) {
+                    $taskIds[] = $task->id;
+                    $taskId = count($taskIds);
+                } else {
+                    $taskId = array_search($task->id, $taskIds);
+                }
+
+                $tasks[$taskId] = $task;
+            }
+
+            // Save back to starage.
+            if (count($taskIds) !== count($clickupTasks)) {
+                $storagePath = STORAGE_DIR . DIRECTORY_SEPARATOR . $matches[5];
+                file_put_contents($storagePath, implode("\n", $taskIds));
+            }
+        }
+
+        $tasks = paginate($tasks, $page, $perPage);
+
+        if (!empty($tasks)) {
+            $response = [];
+            foreach ($tasks as $id => $task) {
+                $response[] = [
+                    'id'          => $id,
+                    'iid'          => $id,
+                    'title'       => sprintf('%s: %s', $task->id, $task->name),
+                    'project_id'  => $matches[5],
+                    'description' => $task->id,
+                    'state'       => 'opened',
+                    'created_at'  => convertToDate($task->date_created),
+                    'updated_at'  => convertToDate($task->date_updated),
+                    'issue_type'  => 'issue',
+                    'web_url'     => $task->url,
+                ];
+            }
+
+            echo json_encode($response);
+        }
+
+        exit;
+    }
+
+    if (count($matches) === 10 && $matches[9] === 'add_spent_time' && isset($_GET['duration'])) {
+        if (!empty($matches[5]) && !empty($matches[8])) {
+            $taskId = getTaskIds($matches[5])[$matches[8]] ?? 0;
+
+            if ($taskId) {
+                preg_match('/(\d+)h\s(\d+)m/', $_GET['duration'], $durationString);
+
+                if (isset($durationString[1]) && isset($durationString[2])) {
+
+                    /**
+                     * (Hours * 60) + Minutes = Minutes
+                     * Minutes * 60 = Seconds
+                     * Seconds * 1000 = Microseconds.
+                     */
+                    $duration = (((int)$durationString[1]*60) + (int)$durationString[2])*60*1000;
+
+                    // Set Space.
+                    $clickup->setSpaceId($matches[5]);
+
+                    $response = $clickup->addTracking($taskId, $duration);
+                    if (!isset($response->ECODE)) {
+                        header('HTTP/1.1 201 Created', true, 201);
+                        exit;
+                    }
+
+                    echo json_encode($response);
+                    exit;
+                }
+
+                echo json_encode(['success' => false, 'message' => 'Nothing to track.']);
+                exit;
+            }
+        }
+
+        echo json_encode(['success' => false, 'message' => 'Task Not Found.']);
+        exit;
+    }
+}
+
+header('HTTP/1.1 404 Not Found');
+exit;
+
+function paginate($data, $page, $perPage): array
+{
+    $offset = ($page - 1)*$perPage;
+
+    return array_slice($data, $offset, $perPage, true);
+}
+
+function convertToDate($timestamp)
+{
+    return date("Y-m-d\TH:i:s.u\Z", round($timestamp/1000));
+}
+
+function getTaskIds($space)
+{
+    // Make storage this if not exists.
+    if (!is_dir(STORAGE_DIR)) {
+        mkdir(STORAGE_DIR);
+    }
+
+    $storagePath = STORAGE_DIR . DIRECTORY_SEPARATOR . $space;
+
+    // Read current storage.
+    $storage = file_get_contents($storagePath);
+
+    // Make an array with current entries.
+    $taskIds = explode("\n", $storage);
+
+    // Filter empty rows and return data.
+    return array_filter($taskIds);
+}
